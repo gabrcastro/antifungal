aaf7f26f0a20ec10acd45b9fef86df10f9397028
gabrcastro <gabrielcastromail@gmail.com>
2025-03-06 11:45:12 -0300

    new design

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const texts = {
-  title: "Revive Your Nails and Radiant Skin Naturally – Try It Today!",
-  subtitle:
-    "Want beautiful, strong nails and healthy skin without harsh chemicals? Discover a natural, at-home treatment using cutting-edge nanotechnology to fight common issues and boost resilience. It’s simple, affordable, and works from the comfort of your home. Click below to explore this easy solution now! Results may vary – consult a professional before use. This is an affiliate review.",
-};
-
-document.getElementById("text_title").textContent = texts.title;
-document.getElementById("text_subtitle").textContent = texts.subtitle;
-
 const encryptedLink =
   "https://e26525m1t4dh5f-ages-m4-m1e.hop.clickbank.net?cbpage=tsl";
 
@@ -14,7 +5,6 @@ document.querySelectorAll("[data-product]").forEach((link) => {
   link.href = encryptedLink;
 });
 
-// Configure o redirecionamento pelo clique na imagem e no texto diretamente
 document.querySelectorAll("[data-product]").forEach((link) => {
   link.addEventListener("click", () => {
     window.open(encryptedLink, "_self");
@@ -34,7 +24,7 @@ document.addEventListener("scroll", () => {
   if (!hasScrolled) {
     if (window.scrollY > 100) {
       redirectToHoplink();
-      hasScrolled = true; // Impede redirecionamentos adicionais
+      hasScrolled = true;
     }
   }
 });
